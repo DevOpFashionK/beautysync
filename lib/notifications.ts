f99761056226d95346f09e2ecd904ddf6f44c604
FashionKillas <jirajd@proton.me>
2026-04-22 01:48:46 -0600

    fix: Tratando de arreglar el cron

--- lib/notifications.ts
+++ lib/notifications.ts
@@ -15,6 +15,12 @@ function getAdminClient() {
   return createClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.SUPABASE_SERVICE_ROLE_KEY!,
+    {
+      auth: {
+        autoRefreshToken: false,
+        persistSession: false,
+      }
+    }
   );
 }
 
